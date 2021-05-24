e4471494d291df29151f07cb03f558b9fe117f47
Eunheui Kim <dmsgml110712@gmail.com>
2021-05-24 18:02:06 +0900

    커밋

---
+++ WinFormsTest/DEV_Form/FM_ITEM.Designer.cs
@@ -0,0 +1,385 @@
+﻿
+namespace DEV_Form
+{
+    partial class FM_ITEM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtItemCode = new System.Windows.Forms.TextBox();
+            this.txtItemName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboItemDesc = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chkNameOnly = new System.Windows.Forms.CheckBox();
+            this.rdoprod = new System.Windows.Forms.RadioButton();
+            this.rdoEnd = new System.Windows.Forms.RadioButton();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dgvGrid = new System.Windows.Forms.DataGridView();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnImDe = new System.Windows.Forms.Button();
+            this.btnImSave = new System.Windows.Forms.Button();
+            this.btnImage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGrid)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "품목 코드";
+            // 
+            // txtItemCode
+            // 
+            this.txtItemCode.Location = new System.Drawing.Point(84, 69);
+            this.txtItemCode.Name = "txtItemCode";
+            this.txtItemCode.Size = new System.Drawing.Size(300, 27);
+            this.txtItemCode.TabIndex = 1;
+            // 
+            // txtItemName
+            // 
+            this.txtItemName.Location = new System.Drawing.Point(477, 69);
+            this.txtItemName.Name = "txtItemName";
+            this.txtItemName.Size = new System.Drawing.Size(300, 27);
+            this.txtItemName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(412, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "품목 명";
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Location = new System.Drawing.Point(899, 69);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(148, 27);
+            this.dtpStart.TabIndex = 4;
+            this.dtpStart.Value = new System.DateTime(2021, 5, 24, 0, 0, 0, 0);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(819, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "출시 일자";
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Location = new System.Drawing.Point(1079, 69);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(148, 27);
+            this.dtpEnd.TabIndex = 8;
+            this.dtpEnd.Value = new System.DateTime(2021, 5, 24, 0, 0, 0, 0);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1053, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "~";
+            // 
+            // cboItemDesc
+            // 
+            this.cboItemDesc.FormattingEnabled = true;
+            this.cboItemDesc.Items.AddRange(new object[] {
+            ""});
+            this.cboItemDesc.Location = new System.Drawing.Point(84, 135);
+            this.cboItemDesc.Name = "cboItemDesc";
+            this.cboItemDesc.Size = new System.Drawing.Size(300, 28);
+            this.cboItemDesc.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(2, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "품목 상세";
+            // 
+            // chkNameOnly
+            // 
+            this.chkNameOnly.AutoSize = true;
+            this.chkNameOnly.Location = new System.Drawing.Point(423, 137);
+            this.chkNameOnly.Name = "chkNameOnly";
+            this.chkNameOnly.Size = new System.Drawing.Size(141, 24);
+            this.chkNameOnly.TabIndex = 12;
+            this.chkNameOnly.Text = "이름으로만 검색";
+            this.chkNameOnly.UseVisualStyleBackColor = true;
+            // 
+            // rdoprod
+            // 
+            this.rdoprod.AutoSize = true;
+            this.rdoprod.Location = new System.Drawing.Point(31, 23);
+            this.rdoprod.Name = "rdoprod";
+            this.rdoprod.Size = new System.Drawing.Size(60, 24);
+            this.rdoprod.TabIndex = 13;
+            this.rdoprod.Text = "생산";
+            this.rdoprod.UseVisualStyleBackColor = true;
+            // 
+            // rdoEnd
+            // 
+            this.rdoEnd.AutoSize = true;
+            this.rdoEnd.Checked = true;
+            this.rdoEnd.Location = new System.Drawing.Point(107, 23);
+            this.rdoEnd.Name = "rdoEnd";
+            this.rdoEnd.Size = new System.Drawing.Size(60, 24);
+            this.rdoEnd.TabIndex = 14;
+            this.rdoEnd.TabStop = true;
+            this.rdoEnd.Text = "단종";
+            this.rdoEnd.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(819, 123);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(117, 40);
+            this.btnSearch.TabIndex = 15;
+            this.btnSearch.Text = "조회";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.txtItemCode);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtItemName);
+            this.groupBox1.Controls.Add(this.chkNameOnly);
+            this.groupBox1.Controls.Add(this.dtpStart);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cboItemDesc);
+            this.groupBox1.Controls.Add(this.dtpEnd);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1299, 212);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "조회";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdoEnd);
+            this.groupBox2.Controls.Add(this.rdoprod);
+            this.groupBox2.Location = new System.Drawing.Point(581, 114);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(196, 60);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "단종 여부";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnDelete);
+            this.groupBox3.Controls.Add(this.btnSave);
+            this.groupBox3.Controls.Add(this.btnAdd);
+            this.groupBox3.Controls.Add(this.dgvGrid);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox3.Location = new System.Drawing.Point(0, 212);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1299, 374);
+            this.groupBox3.TabIndex = 17;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "품목 정보";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(210, 57);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 29);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(110, 57);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 29);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(10, 57);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 29);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dgvGrid
+            // 
+            this.dgvGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGrid.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvGrid.Location = new System.Drawing.Point(3, 119);
+            this.dgvGrid.Name = "dgvGrid";
+            this.dgvGrid.RowHeadersWidth = 51;
+            this.dgvGrid.RowTemplate.Height = 29;
+            this.dgvGrid.Size = new System.Drawing.Size(1293, 252);
+            this.dgvGrid.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnImDe);
+            this.groupBox4.Controls.Add(this.btnImSave);
+            this.groupBox4.Controls.Add(this.btnImage);
+            this.groupBox4.Controls.Add(this.pictureBox1);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(0, 586);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(1299, 436);
+            this.groupBox4.TabIndex = 18;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "품목 이미지 관리";
+            this.groupBox4.Enter += new System.EventHandler(this.groupBox4_Enter);
+            // 
+            // btnImDe
+            // 
+            this.btnImDe.Location = new System.Drawing.Point(274, 195);
+            this.btnImDe.Name = "btnImDe";
+            this.btnImDe.Size = new System.Drawing.Size(94, 29);
+            this.btnImDe.TabIndex = 3;
+            this.btnImDe.Text = "삭제";
+            this.btnImDe.UseVisualStyleBackColor = true;
+            // 
+            // btnImSave
+            // 
+            this.btnImSave.Location = new System.Drawing.Point(174, 195);
+            this.btnImSave.Name = "btnImSave";
+            this.btnImSave.Size = new System.Drawing.Size(94, 29);
+            this.btnImSave.TabIndex = 2;
+            this.btnImSave.Text = "저장";
+            this.btnImSave.UseVisualStyleBackColor = true;
+            // 
+            // btnImage
+            // 
+            this.btnImage.Location = new System.Drawing.Point(6, 195);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(158, 29);
+            this.btnImage.TabIndex = 1;
+            this.btnImage.Text = "이미지 불러오기";
+            this.btnImage.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(8, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 154);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FM_ITEM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 1022);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FM_ITEM";
+            this.Text = "FM_ITEM";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FM_ITEM_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGrid)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtItemCode;
+        private System.Windows.Forms.TextBox txtItemName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboItemDesc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox chkNameOnly;
+        private System.Windows.Forms.RadioButton rdoprod;
+        private System.Windows.Forms.RadioButton rdoEnd;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgvGrid;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnImDe;
+        private System.Windows.Forms.Button btnImSave;
+        private System.Windows.Forms.Button btnImage;
+    }
+}
